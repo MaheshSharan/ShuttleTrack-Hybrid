5374bfde7cd51a93d6f00d2bd504f8fdd4fd7b7d
maheshsharan <maheshsharan28@gmail.com>
2025-05-22 14:57:17 +0530

    fix import error in distrbution test

--- test/check_visibility_distribution.py
+++ test/check_visibility_distribution.py
@@ -1,5 +1,6 @@
 # scripts/check_visibility_distribution.py
-import os
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 from utils.dataset import ShuttleTrackDataset
 
